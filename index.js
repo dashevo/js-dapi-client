a9557ec3718cf674d326e7af500d966739990c7a
Kamuela Franco <kamuela.franco@gmail.com>
2017-11-03 15:43:04 +0000

    Added webpack to npm scripts

---
+++ index.js
@@ -0,0 +1,24 @@
+const Accounts = require('./Accounts');
+const Blockchain = require('./Blockchain');
+const BWS = require('./BWS');
+const Discover = require('./Discover');
+const Explorer = require('./Explorer');
+const SPV = require('./SPV');
+const Wallet = require('./Wallet');
+
+const api = (options) => {
+  if (options) {
+    console.warn('Logging levels have not been implemented yet');
+  }
+  return {
+    Accounts,
+    Blockchain,
+    BWS,
+    Discover,
+    Explorer,
+    SPV,
+    Wallet,
+  };
+};
+
+module.exports = api;

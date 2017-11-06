f411183507320cca0397b5ce9e62d734e106cad7
Kamuela Franco <kamuela.franco@gmail.com>
2017-11-06 21:41:55 +0000

    [EV-526] Added reject case

--- Blockchain/addBlock.js
+++ Blockchain/addBlock.js
@@ -1,22 +1,21 @@
-const addBlock = () =>
-  async blocks =>
-    new Promise((async (resolve, reject) => {
-      let blockHeaders = [];
-      if (!Array.isArray(blocks)) {
-        blockHeaders.push(blocks);
-      } else if (blocks.length > 0) {
-        blockHeaders = blocks;
-      } else {
-        resolve(false);
+const addBlock = blocks =>
+  new Promise((async (resolve, reject) => {
+    let blockHeaders = [];
+    if (!Array.isArray(blocks)) {
+      blockHeaders.push(blocks);
+    } else if (blocks.length > 0) {
+      blockHeaders = blocks;
+    } else {
+      resolve(false);
+    }
+    blockHeaders = blockHeaders.map(blockHeader => this.Blockchain.normalizeHeader(blockHeader));
+    this.Blockchain.chain.addHeaders(blockHeaders, (error) => {
+      if (error) {
+        console.error(error);
+        reject(error);
       }
-      blockHeaders = blockHeaders.map(blockHeader => this.Blockchain.normalizeHeader(blockHeader));
-      this.Blockchain.chain.addHeaders(blockHeaders, (error) => {
-        if (error) {
-          console.error(error);
-          reject(error);
-        }
-        resolve(true);
-      });
-    }));
+      resolve(true);
+    });
+  }));
 
 module.exports = { addBlock };

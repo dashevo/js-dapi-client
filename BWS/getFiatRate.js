5a836ae076b7464901530a409f9430991def5a25
Kamuela Franco <kamuela.franco@gmail.com>
2017-11-21 21:54:36 +0000

    [EV-526] Refactored getFiatRate.js

--- BWS/getFiatRate.js
+++ BWS/getFiatRate.js
@@ -1,8 +1,8 @@
 // TODO: Implement this
 // const { explorerGet } = require('../Common/ExplorerHelper');
 
-const getFiatRate = (opts, ccyCode, ts, provider) =>
-  new Promise(((resolve, reject) =>
+const getFiatRate = (opts, ccyCode, ts) =>
+  new Promise(((resolve) =>
     resolve({ ts: Date.now() - 3000, rate: 120, fetchedOn: Date.now() })));
 
 module.exports = {

dc80162e45ae66725ce84f99bbade207e867c4af
Kamuela Franco <kamuela.franco@gmail.com>
2017-11-21 14:38:47 +0000

    [EV-526] Refactored mnemonic.js

--- util/mnemonic.js
+++ util/mnemonic.js
@@ -1,31 +1,32 @@
-const Mnemonic = {
-  generateMnemonicAndSeed(lang = 'english', passphrase = '') {
-    const bip39 = require('bip39');
+const bip39 = require('bip39');
 
-    const strength = 128;
-    const rng = null;// random number generator
-    const wordlist = bip39.wordlists[lang];
+const generateMnemonicAndSeed = (lang = 'english', passphrase = '') => {
+  const strength = 128;
+  const rng = null;// random number generator
+  const wordlist = bip39.wordlists[lang];
 
-    const mnemonic = bip39.generateMnemonic(strength, rng, wordlist);
-    const valid = bip39.validateMnemonic(mnemonic);
-    if (!valid) { return Error('Invalid mnemonic!'); }
-    const entropy = bip39.mnemonicToEntropy(mnemonic);
-    const seed = bip39.mnemonicToSeedHex(mnemonic, passphrase);
+  const mnemonic = bip39.generateMnemonic(strength, rng, wordlist);
+  const valid = bip39.validateMnemonic(mnemonic);
+  if (!valid) { throw new Error('Invalid mnemonic!'); }
+  const entropy = bip39.mnemonicToEntropy(mnemonic);
+  const seed = bip39.mnemonicToSeedHex(mnemonic, passphrase);
 
-    return {
-      bits: strength,
-      language: lang,
-      seed,
-      entropy,
-      phrase: mnemonic,
-      passphrase,
-
-    };
-  },
-  generateSeedFromMnemonic(mnemonic, passphrase = '') {
-    const bip39 = require('bip39');
-    const seed = bip39.mnemonicToSeedHex(mnemonic, passphrase);
-    return seed;// return HEX seed
-  },
+  return {
+    bits: strength,
+    language: lang,
+    seed,
+    entropy,
+    phrase: mnemonic,
+    passphrase,
+  };
 };
-module.exports = Mnemonic;
+
+const generateSeedFromMnemonic = (mnemonic, passphrase = '') => {
+  const seed = bip39.mnemonicToSeedHex(mnemonic, passphrase);
+  return seed;// return HEX seed
+};
+
+module.exports = {
+  generateMnemonicAndSeed,
+  generateSeedFromMnemonic,
+};

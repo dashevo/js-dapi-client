1026b4134b5971fc67c05ed640952d6d71feaa39
Kamuela Franco <kamuela.franco@gmail.com>
2017-11-21 23:07:14 +0000

    [EV-526] Refactored getBlockChainwork.js

--- Explorer/API/getBlockChainwork.js
+++ Explorer/API/getBlockChainwork.js
@@ -1,7 +1,10 @@
-exports.getBlockChainwork = function (identifier) {
-  return new Promise(((resolve, reject) => SDK.Explorer.API.getBlock(identifier)
+const getBlockChainwork = (identifier, SDK) =>
+  new Promise(((resolve, reject) => SDK.Explorer.API.getBlock(identifier)
     .then((_block) => {
       resolve(_block.chainwork);
     })
     .catch(err => reject(err))));
+
+module.exports = {
+  getBlockChainwork,
 };

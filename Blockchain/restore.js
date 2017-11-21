0932485383eb3279955ba19b9a39c162c6ca2ccb
Kamuela Franco <kamuela.franco@gmail.com>
2017-11-21 14:44:54 +0000

    [EV-526] Refactored restore.js

--- Blockchain/restore.js
+++ Blockchain/restore.js
@@ -1,6 +1,5 @@
-exports.restore = function () {
-  const self = this;
-  return async function (query, update) {
-    return new Promise(((resolve, reject) => resolve(true)));
-  };
+const restore = () => async () => new Promise((resolve => resolve(true)));
+
+module.exports = {
+  restore,
 };

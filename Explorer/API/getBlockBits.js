bdc7187703ee2d4829cb58c50696392d74192cee
Kamuela Franco <kamuela.franco@gmail.com>
2017-11-21 23:04:29 +0000

    [EV-526] Refactored getBlockBits.js

--- Explorer/API/getBlockBits.js
+++ Explorer/API/getBlockBits.js
@@ -1,9 +1,12 @@
-exports.getBlockBits = function (identifier) {
-  return new Promise(((resolve, reject) => SDK.Explorer.API.getBlock(identifier)
+const getBlockBits = (identifier, SDK) =>
+  new Promise(((resolve, reject) => SDK.Explorer.API.getBlock(identifier)
     .then((_block) => {
       resolve(_block.bits);
     })
     .catch((err) => {
       reject(err);
     })));
+
+module.exports = {
+  getBlockBits,
 };

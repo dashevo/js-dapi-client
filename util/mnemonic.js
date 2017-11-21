5a0411f424e27c8c38e2217799275d288b29e6a4
Kamuela Franco <kamuela.franco@gmail.com>
2017-11-21 14:33:12 +0000

    Fixed typographical error in identifier

--- util/mnemonic.js
+++ util/mnemonic.js
@@ -2,18 +2,18 @@ const Mnemonic = {
   generateMnemonicAndSeed(lang = 'english', passphrase = '') {
     const bip39 = require('bip39');
 
-    const strenght = 128;
+    const strength = 128;
     const rng = null;// random number generator
     const wordlist = bip39.wordlists[lang];
 
-    const mnemonic = bip39.generateMnemonic(strenght, rng, wordlist);
+    const mnemonic = bip39.generateMnemonic(strength, rng, wordlist);
     const valid = bip39.validateMnemonic(mnemonic);
     if (!valid) { return Error('Invalid mnemonic!'); }
     const entropy = bip39.mnemonicToEntropy(mnemonic);
     const seed = bip39.mnemonicToSeedHex(mnemonic, passphrase);
 
     return {
-      bits: strenght,
+      bits: strength,
       language: lang,
       seed,
       entropy,

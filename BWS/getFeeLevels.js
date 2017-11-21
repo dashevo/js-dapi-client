48f20bd4f06e28b0cfbe755318f0deb3a127c6bd
Kamuela Franco <kamuela.franco@gmail.com>
2017-11-21 14:51:08 +0000

    [EV-526] Refactored getFeeLevels.js

--- BWS/getFeeLevels.js
+++ BWS/getFeeLevels.js
@@ -1,12 +1,12 @@
-const moment = require('moment');
-const explorerGet = require('../Common/ExplorerHelper').explorerGet/*  */;
-const lastHeight = require('../Explorer/API/getLastBlockHeight').getLastBlockHeight;
+const { explorerGet } = require('../Common/ExplorerHelper');
+// TODO: Implement this
+// const { getLastBlockHeight } = require('../Explorer/API/getLastBlockHeight');
 
-exports.getFeeLevels = function (network, cb) {
-  return new Promise(((resolve, reject) => {
+const getFeeLevels = (network, cb) =>
+  new Promise(((resolve, reject) => {
     explorerGet('/utils/estimatefee')
       .then(res =>
-        // Pick the first value of the first key
+      // Pick the first value of the first key
         res[Object.keys(res)[0]])
       .then((fee) => {
         if (cb) {
@@ -16,4 +16,7 @@ exports.getFeeLevels = function (network, cb) {
       })
       .catch(err => reject(err));
   }));
+
+module.exports = {
+  getFeeLevels,
 };

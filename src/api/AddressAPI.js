e8127bc72854e5f95e9b05b231e64f11ee9cb20d
Anton Suprunchuk <antouhou@gmail.com>
2017-12-05 20:02:25 +0300

    One more style fix

--- src/api/AddressAPI.js
+++ src/api/AddressAPI.js
@@ -2,10 +2,10 @@ const API = require('./BaseAPI');
 
 class AddressApi extends API {
   async getUTXO(address) {
-    return this.makeGetRequest(`/addr/${addr}/utxo`);
+    return this.makeGetRequest(`/addr/${address}/utxo`);
   }
   async getBalance(address) {
-    return this.makeGetRequest(`/addr/${addr}/balance`);
+    return this.makeGetRequest(`/addr/${address}/balance`);
   }
 }
 
--- src/api/UserAPI.js
+++ src/api/UserAPI.js
@@ -1,12 +1,7 @@
 const API = require('./BaseAPI');
 
 class UserApi extends API {
-  async getUTXO(address) {
-    return this.makeGetRequest(`/addr/${addr}/utxo`);
-  }
-  async getBalance(address) {
-    return this.makeGetRequest(`/addr/${addr}/balance`);
-  }
+
 }
 
 module.exports = UserApi;

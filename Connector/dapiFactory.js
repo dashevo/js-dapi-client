097add9cd32ed144f728bce6d5c9a286a433ec52
Anton Suprunchuk <antouhou@gmail.com>
2018-01-04 18:07:56 +0300

    Removed Interfaces folder

--- Connector/dapiFactory.js
+++ Connector/dapiFactory.js
@@ -1,16 +1,14 @@
-const Accounts = require('../Accounts/').Accounts();
-const Explorer = require('../Explorer/').Explorer();
-const Discover = require('../Discover/').Discover();
-const BWS = require('../BWS/').BWS();
-const Blockchain = require('../Blockchain/').Blockchain();
-const SPV = require('../SPV/');
+const Accounts = require('../Accounts').Accounts();
+const Explorer = require('../Explorer').Explorer();
+const Discover = require('../Discover').Discover();
+const Blockchain = require('../Blockchain').Blockchain();
+const SPV = require('../SPV');
 const config = require('../config.js');
 
 module.exports = {
   Accounts,
   Explorer,
   Discover,
-  BWS,
   Blockchain,
   SPV,
   config,
--- index.js
+++ index.js
@@ -3,21 +3,17 @@ const Blockchain = require('./Blockchain');
 const Discover = require('./Discover');
 const Explorer = require('./Explorer');
 const SPV = require('./SPV');
-const Wallet = require('./Wallet');
-const Interfaces = require('./Interfaces');
 
 const api = (options) => {
   if (options) {
     console.warn('Logging levels have not been implemented yet');
   }
   return {
-    Interfaces,
     Accounts,
     Blockchain,
     Discover,
     Explorer,
     SPV,
-    Wallet,
   };
 };
 

0cc7aba922045f993b9b9917febda30ba194d66f
Anton Suprunchuk <antouhou@gmail.com>
2018-03-16 12:09:46 +0300

    [EV-690] Tuned karma; Added babel to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,20 @@ const path = require('path');
 
 const config = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+    ],
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',

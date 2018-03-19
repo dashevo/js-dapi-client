60e1f8dfe8d033964a5e035255eaded82b49e267
Anton Suprunchuk <antouhou@gmail.com>
2018-03-19 20:05:16 +0300

    [EV-691] Added UMD bundling for webpack; Refactored webpack config a bit; Added simple test for bundle

--- test.spec.js
+++ test.spec.js
@@ -1,7 +1,6 @@
-// This file is used for compiling tests with webpack into one file for
-// using with karma
+// This file is used for compiling tests with webpack into one file for using with karma
 require('babel-polyfill');
 
-const testsContext = require.context('./test', true, /\.js$/);
+const testsContext = require.context('./test/src', true, /\.js$/);
 
 testsContext.keys().forEach(testsContext);

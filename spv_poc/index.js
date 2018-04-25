4abcfc6fc6f930f5e492b5a83dfd21035ab85b42
Pierre <pierre@dash.org>
2018-04-25 13:13:57 +0200

    add block header collector

--- spv_poc/index.js
+++ spv_poc/index.js
@@ -3,16 +3,17 @@ const data = require('./data');
 const config = require('../src/config');
 const bloomFilter = require('bloom-filter');
 const dashcore = require('bitcore-lib-dash');
+const { SpvChain } = require('dash-spv');
+// const Merkleproof = require('@dashevo/dash-spv/lib/merkleproof');
 
 const log = console;
+let chain = null;
+let startHeight = 0;
 
 // Setting port to local instance of DAPI.
 // Comment this line if you want to use default port that points to
 // mn-bootstrap
 config.Api.port = 3000;
-
-dashcore.Networks.defaultNetwork = dashcore.Networks.testnet;
-
 const api = new Api();
 
 function createFilter(ref) {
@@ -26,10 +27,40 @@ function createFilter(ref) {
   return filter;
 }
 
-async function main() {
-  await api.loadBloomFilter(createFilter(data.constants.CLIENT_1).toObject());
+function headerCollector() {
+  api.getBestBlockHeight()
+    .then((currHeight) => {
+      const deltaHeight = currHeight - (chain.getChainHeight() + startHeight);
+      if (deltaHeight > 0) {
+        return api.getBlockHeaders(chain.getChainHeight() + startHeight + 1, deltaHeight);
+      }
+
+      return null;
+    }).then((headersObj) => {
+      if (headersObj && headersObj.headers.length > 0) {
+        chain.addHeaders(headersObj.headers);
+      }
+    });
 }
 
+function start() {
+  api.getBestBlockHeight()
+    .then((currHeight) => {
+      startHeight = currHeight;
+      return api.getBlockHeaders(currHeight, 1);
+    })
+    .then((headerObj) => {
+      chain = new SpvChain('custom_genesis', headerObj.headers[0]);
+      setInterval(headerCollector, 1000);
+    });
+}
+
+async function main() {
+  dashcore.Networks.defaultNetwork = dashcore.Networks.testnet;
+  api.loadBloomFilter(createFilter(data.constants.CLIENT_1).toObject());
+
+  start();
+}
 
 main().catch((e) => {
   log.error(e.stack);
--- src/api/index.js
+++ src/api/index.js
@@ -73,6 +73,14 @@ class DAPI {
   getBlockHash(blockHeight) { return this.makeRequestToRandomDAPINode('getBlockHash', [blockHeight]); }
 
   /**
+   * Returns block headers from [offset] with length [limit], where limit is <= 250
+   * @param {number} offset
+   * @param {number} limit
+   * @returns {Promise<[objects]>} - array of header objects
+   */
+  getBlockHeaders(offset, limit) { return this.makeRequestToRandomDAPINode('getBlockHeaders', [offset, limit]); }
+
+  /**
    * ONLY FOR TESTING PURPOSES WITH REGTEST. WILL NOT WORK ON TESTNET/LIVENET.
    * @param {number} amount - Number of blocks to generate
    * @returns {Promise<string[]>} - block hashes

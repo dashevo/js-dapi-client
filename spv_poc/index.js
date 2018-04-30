8d1e1a3c2f7ff55804f2568e98c6f9d9175aca38
Pierre <pierre@dash.org>
2018-04-30 12:53:05 +0200

    tx confirmation status

--- spv_poc/index.js
+++ spv_poc/index.js
@@ -5,14 +5,14 @@ const bloomFilter = require('bloom-filter');
 const dashcore = require('bitcore-lib-dash');
 const { SpvChain } = require('dash-spv');
 const ui = require('log-update');
-// const Merkleproof = require('@dashevo/dash-spv/lib/merkleproof');
+const { MerkleProof } = require('dash-spv');
 
 const log = console;
 
 let chain = null;
 let startHeight = 0;
 let filter = 0;
-let txData = [];
+let txOutput = ``;
 
 // Setting port to local instance of DAPI.
 // Comment this line if you want to use default port that points to
@@ -38,7 +38,6 @@ function headerCollector() {
       if (deltaHeight > 0) {
         return api.getBlockHeaders(chain.getChainHeight() + startHeight + 1, deltaHeight);
       }
-
       return null;
     }).then((headersObj) => {
       if (headersObj && headersObj.headers.length > 0) {
@@ -47,69 +46,85 @@ function headerCollector() {
     });
 }
 
-function outputGenerator() {
-  let txOutput = ``;
-  txData.forEach(tx => {
-    txOutput += tx.txHash + `\n`
-  })
+function getSpvTransactions() {
 
-  ui(`
-      Checkpoint block    : ${startHeight}
+  let txData = [];
 
-      Current block       : ${chain.getChainHeight() + startHeight} (+${chain.getChainHeight()})
-
-      Last block hash     : ${chain.getTipHash()}
-
-      Longest Chain POW   : ${chain.getBestFork().getPOW()}
-
-      ==============================================================================================
-
-      Orphan Chains       : ${chain.getAllForks().length - 1}
-
-      **coming soon**
-
-      ==============================================================================================
-
-      Transactions        : ${txData.length}
-
-      ${txOutput}
-
-`);
-}
-
-function verifyMerkleProof(tx) {
-
-}
-
-function spvCollector() {
-  api.getSpvData(filter)
+  return api.getSpvData(filter)
     .then(data => {
       if (data) {
-
         //get unique tx hashes and push to arr
         data.transactions
           .map(txObj => txObj.hash)
           .filter((txHash, index, self) => self.indexOf(txHash) === index)
-          .map(tx => {
-            txData.push({ 
-              txHash: tx,
-              merkleproof: null,
-              isSpvVerified: false
+          .map(txHash => {
+            txData.push({
+              txHash: txHash,
+              merkleBlock: null,
             })
           })
 
         txData.forEach(tx => {
           //Todo: confirm & improve reversal logic
           let reversedTx = new Buffer(tx.txHash, 'hex').reverse().toString('hex');
-          tx.merkleproof = data.merkleblocks.filter(b => b.hashes.indexOf(reversedTx) > 0)[0]
-
-          if (tx.merkleproof) tx.isSpvVerified = verifyMerkleProof(tx);
+          tx.merkleBlock = data.merkleblocks.filter(mb => mb.hashes.includes(reversedTx))[0];
         })
       }
+
+      return txData;
     })
 }
 
-function start() {
+function writeOutput(transactions) {
+
+  Promise.all(transactions.map(tx => {
+    return new Promise((resolve, reject) => {
+
+      if (tx.merkleBlock) {
+        chain.getBlock(tx.merkleBlock.header.hash)
+          .then(localBlock => {
+            resolve(`
+            ${tx.txHash} ${
+              tx.merkleBlock && localBlock && MerkleProof(tx.merkleBlock, localBlock, tx.txHash) ?
+                "(Confirmed on chain)" : "(UNCONFIRMED)"}`)
+          })
+      }
+      else {
+        resolve(`${tx.txHash} "(UNCONFIRMED!)"}`)
+      }
+
+    })
+  }))
+    .then(txData => {
+      ui(`
+      Checkpoint block    : ${startHeight}
+  
+      Current block       : ${chain.getChainHeight() + startHeight} (+${chain.getChainHeight()})
+  
+      Last block hash     : ${chain.getTipHash()}
+  
+      Longest Chain POW   : ${chain.getBestFork().getPOW()}
+  
+      ==============================================================================================
+  
+      Orphan Chains       : ${chain.getAllForks().length - 1}
+  
+      ==============================================================================================
+  
+      Transactions        : ${txData}`);
+    })
+}
+
+function outputGenerator() {
+  getSpvTransactions()
+    .then(transactions => writeOutput(transactions))
+}
+
+async function main() {
+  dashcore.Networks.defaultNetwork = dashcore.Networks.testnet;
+  filter = createFilter(data.constants.CLIENT_1).toObject();
+  api.loadBloomFilter(filter);
+
   process.stdout.write('\033c');
   api.getBestBlockHeight()
     .then((currHeight) => {
@@ -118,18 +133,9 @@ function start() {
     })
     .then((headerObj) => {
       chain = new SpvChain('custom_genesis', headerObj.headers[0]);
-      setInterval(headerCollector, 10000);
-      setInterval(outputGenerator, 5000);
-      setTimeout(spvCollector, 1000);
+      headerCollector(); setInterval(headerCollector, 10000);
+      setInterval(outputGenerator, 2000);
     });
-}
-
-async function main() {
-  dashcore.Networks.defaultNetwork = dashcore.Networks.testnet;
-  filter = createFilter(data.constants.CLIENT_1).toObject();
-  api.loadBloomFilter(filter);
-
-  start();
 }
 
 main().catch((e) => {

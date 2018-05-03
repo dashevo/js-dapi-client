1081007a0ec14090e83202b994eeeba3ea399990
Anton Suprunchuk <antouhou@gmail.com>
2018-05-03 22:22:35 +0300

    Add dap creation and object creation to example

--- examples/index.js
+++ examples/index.js
@@ -1,15 +1,23 @@
 const registerUser = require('./register_user');
 const topupUserCredit = require('./topup_user_credits');
+const createDapContract = require('./register_dap');
+const updateUserState = require('./update_user_state');
+const Schema = require('@dashevo/dash-schema/lib');
 const Api = require('../src/api');
+const Bitcore = require('@dashevo/dashcore-lib');
 const { privateKeyString } = require('./data');
-const config = require('../src/config');
+
+const HDKey = '';
+// const config = require('../src/config');
 
 const log = console;
 
 // Setting port to local instance of DAPI.
 // Comment this line if you want to use default port that points to
 // mn-bootstrap
-config.Api.port = 3000;
+// config.Api.port = 3001;
+
+const dashPayId = '';
 
 const api = new Api();
 
@@ -18,7 +26,7 @@ async function main() {
   const username = Math.random().toString(36).substring(7);
   log.info('Your random username for this run is:');
   log.info(username);
-  // Sending registration to network
+  // Sending registration to the network
   // Note: in this example we assume that account owner is the same
   // person who funds registration, so only one private key is used.
   // Otherwise, owner should create registration transaction and
@@ -39,6 +47,65 @@ async function main() {
   // Check user data
   blockchainUser = await api.getUser(username);
   log.info('User credits after top up:', blockchainUser.credits);
+
+  // Registering dap, if it's not registered already:
+  let dashPayDataContract = await api.getDapContract(dashPayId);
+
+  if (!dashPayDataContract) {
+    log.info('DashPay data contract not found. Creating one');
+    const contract = Schema.create.dapcontract(Schema.Daps.DashPay);
+    await createDapContract(contract);
+    // Confirming dap contract creation on-chain
+    await api.generate(1);
+    // Checking if it's really created
+    dashPayDataContract = await api.getDapContract(dashPayId);
+  }
+
+  log.info('DashPay data contract:');
+  log.info(dashPayDataContract);
+
+  // This code is DashPay-specific.
+
+  // Creating "contact" object
+  const contactRequest = Schema.create.dapobject('contact');
+
+  // Registering other user to which we want to send friend request
+  const otherUserUsername = Math.random().toString(36).substring(7);
+  const otherUserId = await registerUser(otherUserUsername, privateKeyString);
+
+  // Generate an HD public key for the user
+  contactRequest.contact.hdextpubkey = new Bitcore
+    .HDPrivateKey(HDKey)
+    .derive('m/1').hdPublicKey.toString();
+  // Setting a relation to that user in object. Later this user can retrieve this object
+  // from DAPI with getDapContext
+  contactRequest.contact.relation = otherUserId;
+
+  const objects = [contactRequest];
+
+  // End of DashPay-specific code.
+
+  await updateUserState(dashPayId, objects, privateKeyString);
+
+  // Generate 1 block to confirm transition
+  await api.generate(1);
+
+  // Check first user dap space - contact request should appear there:
+
+  const user1Space = await api.getUserDapSpace(dashPayId, blockchainUser.regTxId);
+
+  log.info('First user dap space:');
+  log.info(user1Space);
+
+  // Check second user dap context - friend request should appear there:
+
+  const user2Context = await api.getUserDapContext(dashPayId, otherUserId);
+  log.info('Second user dap space:');
+  log.info(user2Context);
+
+  // Now we need to accept first user contact request, i.e. create contact object
+  // on second user side, referencing first user's id:
+
   process.exit(0);
 }
 

16cc0c662ad7f3f27d06c050ddaa04c0491ed44b
Pierre <pierre@dash.org>
2018-09-03 12:15:41 +0200

    newlines

--- examples/nodeDiscovery.js
+++ examples/nodeDiscovery.js
@@ -10,15 +10,15 @@ const deletedMnHash = '00000f5da94df7df6d8715e149467a5e859fe5db48366a68ab94dadc3
 
 getVerfiedMnList(nullhash, [], allMnsActiveAtHash)
   .then((mnList) => {
-    log.info('\t MnList @ block 2896 - 3 mns valid:');
+    log.info('\nMnList @ block 2896 - 3 mns valid:');
     log.info(JSON.stringify(mnList, null, 4));
     return getVerfiedMnList(allMnsActiveAtHash, mnList, deletedMnHash);
   }).then((mnList) => {
-    log.info('\t MnList @ block 2897 - 2 mns valid, 1 removed:');
+    log.info('\nMnList @ block 2897 - 2 mns valid, 1 removed:');
     log.info(JSON.stringify(mnList, null, 4));
     return getVerfiedMnList(deletedMnHash, mnList);
   }).then((mnList) => {
-    log.info('\t MnList @ latest block - deleted mn added back at block 2904:');
+    log.info('\nMnList @ latest block - deleted mn added back at block 2904:');
     log.info(JSON.stringify(mnList, null, 4));
 
     // Keep checking for updates to at each block

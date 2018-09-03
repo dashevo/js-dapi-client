3df0d1394f0460f3f3607118b53d9833d5576864
Pierre <pierre@dash.org>
2018-09-03 07:45:26 +0200

    getVerfiedMnList helper

---
+++ src/Helpers/getVerfiedMnList.js
@@ -0,0 +1,65 @@
+const Client = require('../');
+// Add this code back when proofs available.
+// const { MerkleProof } = require('dash-spv');
+
+const client = new Client();
+
+function getBestBlockHash() {
+  return client.getBestBlockHeight()
+    .then(height => client.getBlockHash(height));
+}
+
+function constructMnList(originalMnList, latestMnList) {
+  return originalMnList
+    .filter(mn => !latestMnList.deletedMNs.includes(mn.proRegTxHash))
+    .concat(latestMnList.mnList)
+    .sort((itemA, itemB) => itemA.proRegTxHash > itemB);
+}
+
+function validateDiffListProofs(mnlistDiff, header, newList) {
+  // Todo: pending core RPC bug currently not returning these proofs
+  // rem next line when proofs available
+  return mnlistDiff && header && newList;
+
+  // Add this code back when proofs available.
+  // return MerkleProof.validateMnProofs(
+  //   header,
+  //   mnlistDiff.merkleFlags,
+  //   mnlistDiff.merkleHashes,
+  //   mnlistDiff.totalTransactions,
+  //   mnlistDiff.cbTx.hash,
+  // ) &&
+  // MerkleProof.validateMnListMerkleRoot(mnlistDiff.mnlistMerkleRoot, newList);
+}
+
+// Todo: pending core RPC bug currently not returning these proofs
+/* eslint-disable no-param-reassign */
+function stubDiffList(mnListDiff) {
+  mnListDiff.totalTransactions = 0;
+  mnListDiff.merkleHashes = [];
+  mnListDiff.merkleFlags = [];
+  mnListDiff.cbTx = {};
+
+  return mnListDiff;
+}
+
+const nullhash = '0000000000000000000000000000000000000000000000000000000000000000';
+async function getVerfiedMnList(lastSyncedHash = nullhash, lastSyncedMnList = []) {
+  // Todo: not ideal pattern (think some dapi architecture needs to change to improve)
+  const bestHash = await getBestBlockHash();
+  const latestHeader = await client.getBlockHeader(bestHash);
+  const candidateList = await client.getMnListDiff(lastSyncedHash, bestHash);
+  const newList = constructMnList(lastSyncedMnList, candidateList);
+
+  // todo: remove
+  stubDiffList(candidateList);
+
+  while (!validateDiffListProofs(candidateList, latestHeader, newList)) {
+    // Todo get new mnlist from different node until a list is obtained
+    // which does validate correctly
+  }
+  return newList;
+}
+
+module.exports = getVerfiedMnList;
+

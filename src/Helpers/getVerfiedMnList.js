dfe237cca83f0e3e6a32044629beb0c8b35bbe83
Pierre <pierre@dash.org>
2018-09-07 07:57:46 +0200

    update helper difflist

--- src/Helpers/getVerfiedMnList.js
+++ src/Helpers/getVerfiedMnList.js
@@ -37,12 +37,13 @@ function validateDiffListProofs(mnlistDiff, header, newList) {
 async function getVerfiedMnList(offSetHash, lastSyncedMnList, _targetHash) {
   const targetHash = _targetHash || await getBestBlockHash();
   const refHeader = await client.getBlockHeader(targetHash);
-  const candidateDiffList = await client.getMnListDiff(offSetHash, targetHash);
-  const newList = constructMnList(lastSyncedMnList, candidateDiffList);
+  const difflist = await client.getMnListDiff(offSetHash, targetHash);
+  const newList = constructMnList(lastSyncedMnList, difflist);
 
   return {
     mnList: newList,
-    valid: validateDiffListProofs(candidateDiffList, refHeader, newList),
+    valid: validateDiffListProofs(difflist, refHeader, newList),
+    cbTxHash: '//todo pending core bug: difflist.cbTx.hash',
     targetHash,
   };
 }

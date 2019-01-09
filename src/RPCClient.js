252ad2c2e954ab034a5f78e0055f2a6bf27a8cf6
Cofresi <cofresi@dash.org>
2019-01-09 14:58:48 -0400

    fix eslint errors

--- src/RPCClient.js
+++ src/RPCClient.js
@@ -11,19 +11,18 @@ const defaultHost = '127.0.0.1';
  * @returns {Promise<*>}
  */
 async function request(url, method, params, options = {}) {
-  const destination = url.host ?
-    `http://${url.host ? url.host : defaultHost}:${url.port ? url.port : ''}` : url;
+  const destination = url.host
+    ? `http://${url.host ? url.host : defaultHost}:${url.port ? url.port : ''}` : url;
   const payload = {
     jsonrpc: '2.0',
     method,
     params,
     id: 1,
   };
-  const res =
-    options.timeout ?
-      await axios({
-        method: 'post', url: destination, data: payload, timeout: options.timeout,
-      }) : await axios.post(destination, payload);
+  const res = options.timeout
+    ? await axios({
+      method: 'post', url: destination, data: payload, timeout: options.timeout,
+    }) : await axios.post(destination, payload);
   if (res.status !== 200) {
     throw new Error(res.statusMessage);
   }

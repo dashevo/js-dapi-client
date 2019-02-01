c7148cf7a95d4fd3305196edf00486d2de4fe24f
Cofresi <cofresi@dash.org>
2019-02-01 14:31:12 -0400

    add interface getBestBlockHash

--- src/index.js
+++ src/index.js
@@ -107,6 +107,12 @@ class DAPIClient {
   getBalance(address) { return this.makeRequestToRandomDAPINode('getBalance', { address }); }
 
   /**
+   * Returns best block hash or block hash of chaintip
+   * @returns {Promise<string>}
+   */
+  getBestBlockHash() { return this.makeRequestToRandomDAPINode('getBestBlockHash', {}); }
+
+  /**
    * Returns best block height
    * @returns {Promise<number>}
    */

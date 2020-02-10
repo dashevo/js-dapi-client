649f20d299b8e57638895930fa3d52ccee57a94e
Ivan Shumkov <ivan@shumkov.ru>
2020-02-10 11:29:54 +0300

    define valid address summary

--- test/src/index.js
+++ test/src/index.js
@@ -42,6 +42,20 @@ chai.use(sinonChai);
 const { expect } = chai;
 
 const validAddressWithOutputs = 'yXdxAYfK8eJgQmHpUzMaKEBhqwKQWKSezS';
+const validAddressSummary = {
+  'addrStr': validAddressWithOutputs,
+  'balance': 4173964.74940914,
+  'balanceSat': 417396474940914,
+  'totalReceived': 4287576.24940914,
+  'totalReceivedSat': 428757624940914,
+  'totalSent': 113611.5,
+  'totalSentSat': 11361150000000,
+  'unconfirmedBalance': 0,
+  'unconfirmedBalanceSat': 0,
+  'unconfirmedTxApperances': 0,
+  'txApperances': 27434,
+  'transactions': ['4f46066bd50cc2684484407696b7949e82bd906ea92c040f59a97cba47ed8176', '8890a0ee95a17f6723ab2d9a0bdd579351b9220738ad34f5b49cbe63f09b082a']
+};
 const contractId = '11c70af56a763b05943888fa3719ef56b3e826615fdda2d463c63f4034cb861c';
 
 const contract = {

eba52aa9e029f30e48e1fa9f31fbf94b5543ad71
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2020-06-03 16:00:20 +0300

    Integration tests Bugfixes

--- test/unit/methods/core/getMnListDiffFactory.spec.js
+++ test/unit/methods/core/getMnListDiffFactory.spec.js
@@ -1,5 +1,7 @@
 const getMnListDiffFactory = require('../../../../lib/methods/core/getMnListDiffFactory');
 
+const getMNListDiffsFixture = require('../../../../lib/test/fixtures/getMNListDiffsFixture');
+
 describe('getMnListDiff', () => {
   let getMnListDiff;
   let jsonRpcTransportMock;
@@ -10,12 +12,7 @@ describe('getMnListDiff', () => {
   beforeEach(function beforeEach() {
     baseBlockHash = '0000047d24635e347be3aaaeb66c26be94901a2f962feccd4f95090191f208c1';
     blockHash = '000000000b0339e07bce8b3186a6a57a3c45d10e16c4bce18ef81b667bc822b2';
-    mnListDiff = {
-      baseBlockHash,
-      blockHash,
-      deletedMNs: [],
-      mnList: [],
-    };
+    mnListDiff = getMNListDiffsFixture();
 
     jsonRpcTransportMock = {
       request: this.sinon.stub().resolves(mnListDiff),

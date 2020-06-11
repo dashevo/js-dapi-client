f8c612f5b4187573c27121534059fc34d4d4f972
Djavid Gabibiyan <djavidoff@gmail.com>
2020-06-11 19:46:52 +0500

    fix: JSON RPC doesn't retry on `ETIMEDOUT (#155)

--- src/transport/JsonRpcTransport.js
+++ src/transport/JsonRpcTransport.js
@@ -38,7 +38,7 @@ class JsonRpcTransport {
 
       return result;
     } catch (e) {
-      if (e.code !== 'ECONNABORTED' && e.code !== 'ECONNREFUSED'
+      if (e.code !== 'ECONNABORTED' && e.code !== 'ECONNREFUSED' && e.code !== 'ETIMEDOUT'
             && e.code !== -32603 && !(e.code >= -32000 && e.code <= -32099)) {
         throw e;
       }

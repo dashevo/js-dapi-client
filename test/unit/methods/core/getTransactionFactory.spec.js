f26c73fc6471df40ff330ee207bc681332404e47
Djavid Gabibiyan <djavidoff@gmail.com>
2021-07-02 18:13:40 +0500

    chore: update test cases

--- test/unit/methods/core/getTransactionFactory.spec.js
+++ test/unit/methods/core/getTransactionFactory.spec.js
@@ -1,7 +1,7 @@
 const {
   v0: {
     GetTransactionRequest,
-    GetTransactionResponse,
+    GetTransactionResponse: ProtoGetTransactionResponse,
     CorePromiseClient,
   },
 } = require('@dashevo/dapi-grpc');
@@ -9,6 +9,8 @@ const {
 const grpcErrorCodes = require('@dashevo/grpc-common/lib/server/error/GrpcErrorCodes');
 
 const getTransactionFactory = require('../../../../lib/methods/core/getTransaction/getTransactionFactory');
+const GetTransactionResponse = require('../../../../lib/methods/core/getTransaction/GetTransactionResponse');
+const NotFoundError = require('../../../../lib/methods/errors/NotFoundError');
 
 describe('getTransactionFactory', () => {
   let getTransaction;
@@ -18,7 +20,7 @@ describe('getTransactionFactory', () => {
   beforeEach(function beforeEach() {
     transaction = Buffer.from('transaction');
 
-    const response = new GetTransactionResponse();
+    const response = new ProtoGetTransactionResponse();
     response.setTransaction(transaction);
 
     grpcTransportMock = {
@@ -39,8 +41,8 @@ describe('getTransactionFactory', () => {
     const request = new GetTransactionRequest();
     request.setId(id);
 
-    expect(result).to.be.instanceof(Buffer);
-    expect(result).to.deep.equal(transaction);
+    expect(result).to.be.instanceof(GetTransactionResponse);
+    expect(result.getTransaction()).to.deep.equal(transaction);
     expect(grpcTransportMock.request).to.be.calledOnceWithExactly(
       CorePromiseClient,
       'getTransaction',
@@ -57,22 +59,16 @@ describe('getTransactionFactory', () => {
 
     const id = '4f46066bd50cc2684484407696b7949e82bd906ea92c040f59a97cba47ed8176';
 
-    const result = await getTransaction(id);
-
-    const request = new GetTransactionRequest();
-    request.setId(id);
-
-    expect(result).to.equal(null);
-    expect(grpcTransportMock.request).to.be.calledOnceWithExactly(
-      CorePromiseClient,
-      'getTransaction',
-      request,
-      {},
-    );
+    try {
+      await getTransaction(id);
+      expect.fail('should throw not found error');
+    } catch (e) {
+      expect(e).to.be.an.instanceOf(NotFoundError);
+    }
   });
 
   it('should return null if transaction is not found', async () => {
-    const response = new GetTransactionResponse();
+    const response = new ProtoGetTransactionResponse();
     grpcTransportMock.request.resolves(response);
 
     const id = '4f46066bd50cc2684484407696b7949e82bd906ea92c040f59a97cba47ed8176';
@@ -82,7 +78,7 @@ describe('getTransactionFactory', () => {
     const request = new GetTransactionRequest();
     request.setId(id);
 
-    expect(result).to.equal(null);
+    expect(result.getTransaction()).to.equal(null);
     expect(grpcTransportMock.request).to.be.calledOnceWithExactly(
       CorePromiseClient,
       'getTransaction',

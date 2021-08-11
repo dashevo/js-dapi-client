245895877298171fd0f14d6938d114e655fbb597
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2021-08-11 17:44:12 +0300

    fix: PR comments

--- test/unit/methods/platform/getConsensusParams/getConsensusParamsResponse.spec.js
+++ test/unit/methods/platform/getConsensusParams/getConsensusParamsResponse.spec.js
@@ -1,12 +1,14 @@
 const {
   v0: {
-    GetConsensusParamsResponse,
-    ConsensusParamsBlock,
-    ConsensusParamsEvidence,
+    GetConsensusParamsResponse: GetConsensusParamsResponseProto,
+    ConsensusParamsBlock: ConsensusParamsBlockProto,
+    ConsensusParamsEvidence: ConsensusParamsEvidenceProto,
   },
 } = require('@dashevo/dapi-grpc');
-const GetConsensusParamsResponseClass = require('../../../../../lib/methods/platform/getConsensusParams/getConsensusParamsResponse');
+const GetConsensusParamsResponse = require('../../../../../lib/methods/platform/getConsensusParams/getConsensusParamsResponse');
 const InvalidResponseError = require('../../../../../lib/methods/platform/response/errors/InvalidResponseError');
+const ConsensusParamsBlock = require('../../../../../lib/methods/platform/getConsensusParams/ConsensusParamsBlock');
+const ConsensusParamsEvidence = require('../../../../../lib/methods/platform/getConsensusParams/ConsensusParamsEvidence');
 
 describe('getConsensusParamsResponse', () => {
   let getConsensusParamsResponse;
@@ -26,7 +28,7 @@ describe('getConsensusParamsResponse', () => {
       },
     };
 
-    getConsensusParamsResponse = new GetConsensusParamsResponseClass(
+    getConsensusParamsResponse = new GetConsensusParamsResponse(
       consensusParamsFixture.block,
       consensusParamsFixture.evidence,
     );
@@ -35,41 +37,63 @@ describe('getConsensusParamsResponse', () => {
   it('should return block', () => {
     const block = getConsensusParamsResponse.getBlock();
 
-    expect(block).to.deep.equal(consensusParamsFixture.block);
+    expect(block).to.be.an.instanceOf(ConsensusParamsBlock);
+    expect(block.getMaxBytes()).to.deep.equal(consensusParamsFixture.block.maxBytes);
+    expect(block.getMaxGas()).to.deep.equal(consensusParamsFixture.block.maxGas);
+    expect(block.getTimeIotaMs()).to.deep.equal(consensusParamsFixture.block.timeIotaMs);
   });
 
   it('should return evidence', () => {
     const evidence = getConsensusParamsResponse.getEvidence();
 
-    expect(evidence).to.deep.equal(consensusParamsFixture.evidence);
+    expect(evidence).to.be.an.instanceOf(ConsensusParamsEvidence);
+    expect(evidence.getMaxAgeNumBlocks())
+      .to.deep.equal(consensusParamsFixture.evidence.maxAgeNumBlocks);
+    expect(evidence.getMaxAgeDuration())
+      .to.deep.equal(consensusParamsFixture.evidence.maxAgeDuration);
+    expect(evidence.getMaxBytes())
+      .to.deep.equal(consensusParamsFixture.evidence.maxBytes);
   });
 
   it('should create an instance from proto', () => {
-    const block = new ConsensusParamsBlock();
+    const block = new ConsensusParamsBlockProto();
     block.setMaxBytes(consensusParamsFixture.block.maxBytes);
     block.setMaxGas(consensusParamsFixture.block.maxGas);
     block.setTimeIotaMs(consensusParamsFixture.block.timeIotaMs);
 
-    const evidence = new ConsensusParamsEvidence();
+    const evidence = new ConsensusParamsEvidenceProto();
     evidence.setMaxAgeNumBlocks(consensusParamsFixture.evidence.maxAgeNumBlocks);
     evidence.setMaxAgeDuration(consensusParamsFixture.evidence.maxAgeDuration);
     evidence.setMaxBytes(consensusParamsFixture.evidence.maxBytes);
 
-    const proto = new GetConsensusParamsResponse();
+    const proto = new GetConsensusParamsResponseProto();
     proto.setBlock(block);
     proto.setEvidence(evidence);
 
-    getConsensusParamsResponse = GetConsensusParamsResponseClass.createFromProto(proto);
+    getConsensusParamsResponse = GetConsensusParamsResponse.createFromProto(proto);
 
-    expect(getConsensusParamsResponse.getBlock()).to.deep.equal(consensusParamsFixture.block);
-    expect(getConsensusParamsResponse.getEvidence()).to.deep.equal(consensusParamsFixture.evidence);
+    expect(getConsensusParamsResponse.getBlock()).to.be.an.instanceOf(ConsensusParamsBlock);
+    expect(getConsensusParamsResponse.getBlock().getMaxBytes())
+      .to.deep.equal(consensusParamsFixture.block.maxBytes);
+    expect(getConsensusParamsResponse.getBlock().getMaxGas())
+      .to.deep.equal(consensusParamsFixture.block.maxGas);
+    expect(getConsensusParamsResponse.getBlock().getTimeIotaMs())
+      .to.deep.equal(consensusParamsFixture.block.timeIotaMs);
+
+    expect(getConsensusParamsResponse.getEvidence()).to.be.an.instanceOf(ConsensusParamsEvidence);
+    expect(getConsensusParamsResponse.getEvidence().getMaxAgeNumBlocks())
+      .to.deep.equal(consensusParamsFixture.evidence.maxAgeNumBlocks);
+    expect(getConsensusParamsResponse.getEvidence().getMaxAgeDuration())
+      .to.deep.equal(consensusParamsFixture.evidence.maxAgeDuration);
+    expect(getConsensusParamsResponse.getEvidence().getMaxBytes())
+      .to.deep.equal(consensusParamsFixture.evidence.maxBytes);
   });
 
   it('should return InvalidResponseError if consensus params are not defined', () => {
-    const proto = new GetConsensusParamsResponse();
+    const proto = new GetConsensusParamsResponseProto();
 
     try {
-      getConsensusParamsResponse = GetConsensusParamsResponseClass.createFromProto(proto);
+      getConsensusParamsResponse = GetConsensusParamsResponse.createFromProto(proto);
 
       expect.fail('should throw InvalidResponseError');
     } catch (e) {
